5c4ed69088364d7d94e7dbfac33cae4b1a61934e
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-02 21:13:36 +0500

    Add project files.

---
+++ EntityDatabase-First/Models/DapperContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityDatabase_First.Models;
+
+public partial class DapperContext : DbContext
+{
+    public DapperContext()
+    {
+    }
+
+    public DapperContext(DbContextOptions<DapperContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server = DESKTOP-02F3BCI; Database = Dapper; Trusted_Connection = True; TrustServerCertificate = True; Pooling = True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.Year).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EntityDatabase-First/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityDatabase_First.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? Lastname { get; set; }
+
+    public DateTime? Year { get; set; }
+}
